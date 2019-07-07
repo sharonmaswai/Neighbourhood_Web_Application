3a961b39ac6f9b1f3a3c8b338078b7f701120b6c
sharonmaswai <chepmaswai@gmail.com>
2019-07-07 17:21:20 +0300

    create static files

--- hood/settings.py
+++ hood/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hoood',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
--- hood/urls.py
+++ hood/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include ('hoood.urls')),
+     url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
